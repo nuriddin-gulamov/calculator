436d92d77ab5e7eefc07401b3f05c2a08b0b7a19
Nuriddin <90389697+nuriddin-gulamov@users.noreply.github.com>
2021-09-19 15:49:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Light Dark Mode
 document.getElementById("dark-mode").style.display = "inline";
 document.getElementById("dark-mode").onclick = () => {
@@ -9,7 +8,7 @@ document.getElementById("dark-mode").onclick = () => {
     document.getElementsByClassName("current-operand")[0].classList.add("current-operand-dark");
     for(var i = 0; i < document.getElementsByClassName("button").length; i++) {
         document.getElementsByClassName("button")[i].classList.add("button-dark");
-    }
+    };
     for(var t = 0; t < document.getElementsByClassName("button-dark").length; t++) {
         document.getElementsByClassName("button-dark")[t].onclick = function () {
             this.classList.add("button-dark-click");
@@ -18,8 +17,8 @@ document.getElementById("dark-mode").onclick = () => {
             }, 133);
             var clickSound = new Audio("./clicksound.mp3");
             clickSound.play();
-        }
-    }
+        };
+    };
 
     document.getElementById("light-mode").onclick = () => {
         document.getElementById("light-mode").style.display = "none";
@@ -29,7 +28,7 @@ document.getElementById("dark-mode").onclick = () => {
         document.getElementsByClassName("current-operand")[0].classList.remove("current-operand-dark");
         for(var i = 0; i < document.getElementsByClassName("button").length; i++) {
             document.getElementsByClassName("button")[i].classList.remove("button-dark");
-        }
+        };
 
         for(var t = 0; t < document.getElementsByClassName("button").length; t++) {
             document.getElementsByClassName("button")[t].onclick = function () {
@@ -39,10 +38,10 @@ document.getElementById("dark-mode").onclick = () => {
                 }, 133);
                 var clickSound = new Audio("./clicksound.mp3");
                 clickSound.play();
-            }
-        }
-    }
-}
+            };
+        };
+    };
+};
 
 for(var t = 0; t < document.getElementsByClassName("button").length; t++) {
     document.getElementsByClassName("button")[t].onclick = function () {
@@ -52,8 +51,8 @@ for(var t = 0; t < document.getElementsByClassName("button").length; t++) {
         }, 133);
         var clickSound = new Audio("./clicksound.mp3");
         clickSound.play();
-    }
-}
+    };
+};
 
 // Variables
 const previousOperandTextElement = document.querySelector("[data-previous-operand]");
@@ -70,37 +69,37 @@ class Calculator {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
         this.clear();
-    }
+    };
 
     clear() {
         this.currentOperand = "";
         this.previousOperand = "";
         this.operation = undefined;
-    }
+    };
 
     delete() {
         this.currentOperand = this.currentOperand.slice(0, -1);
-    }
+    };
 
     appendNumber(number) {
         if (number === "." && this.currentOperand.includes(".")) return;
         this.currentOperand = this.currentOperand.toString() + number.toString();
-    }
+    };
 
     chooseOperation(operation) {
         if (this.currentOperand === "") return;
         if (this.previousOperand !== "") {
             this.calculate();
-        }
+        };
         this.previousOperand = this.currentOperand + operation;
         this.currentOperand = "";
         this.operation = operation;
-    }
+    };
 
     calculate() {
-        let result
+        let result;
         var prev = parseFloat(this.previousOperand);
-        var current = parseFloat(this.currentOperand    );
+        var current = parseFloat(this.currentOperand);
         switch (this.operation) {
             case "/":
                 result = prev / current;
@@ -119,17 +118,17 @@ class Calculator {
                 break;
                 
             default: return;
-        }
+        };
         this.previousOperand = "";
         this.currentOperand = result;
         this.operation = undefined;
-    }
+    };
 
     displayUp() {
         this.currentOperandTextElement.innerText = this.currentOperand;
         this.previousOperandTextElement.innerText = this.previousOperand;
-    }
-}
+    };
+};
 
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
 
@@ -154,10 +153,92 @@ operations.forEach(button => {
     button.addEventListener("click", () => {
         calculator.chooseOperation(button.innerText);
         calculator.displayUp();
-    })
-})
+    });
+});
 
 equals.addEventListener("click", () => {
     calculator.calculate();
     calculator.displayUp();
+});
+
+document.addEventListener("keydown", function (event) {
+    switch (event.key) {
+        case "0":
+            document.getElementsByClassName("button")[16].click();
+            break;
+        
+        case "1":
+            document.getElementsByClassName("button")[3].click();
+            break;
+        
+        case "2":
+            document.getElementsByClassName("button")[4].click();
+            break;
+        
+        case "3":
+            document.getElementsByClassName("button")[5].click();
+            break;
+        
+        case "4":
+            document.getElementsByClassName("button")[7].click();
+            break;
+        
+        case "5":
+            document.getElementsByClassName("button")[8].click();
+            break;
+        
+        case "6":
+            document.getElementsByClassName("button")[9].click();
+            break;
+        
+        case "7":
+            document.getElementsByClassName("button")[11].click();
+            break;
+        
+        case "8":
+            document.getElementsByClassName("button")[12].click();
+            break;
+        
+        case "9":
+            document.getElementsByClassName("button")[13].click();
+            break;
+        
+        case ".":
+            document.getElementsByClassName("button")[15].click();
+            break;
+        
+        
+        case "/":
+            document.getElementsByClassName("button")[2].click();
+            break;
+        
+        case "*":
+            document.getElementsByClassName("button")[6].click();
+            break;
+        
+        case "+":
+            document.getElementsByClassName("button")[10].click();
+            break;
+        
+        case "-":
+            document.getElementsByClassName("button")[14].click();
+            break;
+        
+
+        case "Delete":
+            document.getElementsByClassName("button")[0].click();
+            break;
+        
+        case "Backspace":
+            document.getElementsByClassName("button")[1].click();
+            break;
+        
+        case "Enter":
+            document.getElementsByClassName("button")[17].click();
+            break;
+        
+        default: return;
+    }
 })
+
+console.log(document.getElementsByClassName("button"));
